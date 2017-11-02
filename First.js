77925543835dc09e12c1535aa2799e398eb3f859
madhavthatavarthi <madhav.thatavarthi@gmail.com>
2017-11-02 17:59:15 +0530

    Updated

--- First.js
+++ First.js
@@ -1,6 +1,9 @@
-var http = require('https');
+var http = require('http');
+var text = require('./Sample.js');    //If sample.js file present in sam path
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World Madhav!');
-}).listen(8080);
+    res.write(text.sampleText());      // calls sample text function in Sample.js file
+    res.end();
+
+}).listen(8000);
